c337b54d9b3dfef5e187d7660697fbb53b28f22c
aitoehigie <aitoehigie@gmail.com>
2019-03-14 14:57:53 +0100

    Adds Doc strings to functions in the utils/tools file

--- ghibil/utils/tools.py
+++ ghibil/utils/tools.py
@@ -3,6 +3,8 @@ import requests
 BASE_URL = "https://ghibliapi.herokuapp.com/"
 
 def fetch_movies_and_actors():
+    '''The below is a nifty list comprehension that retrieves all information from the films API endpoint and makes a GET request to retrieve information about all actors in each film'''
+
     results = [(item["title"], [requests.get(i).json()["name"] for i in item["people"] if len(item["people"]) > 1]) for item in requests.get("https://ghibliapi.herokuapp.com/films").json()] 
     return results
 
